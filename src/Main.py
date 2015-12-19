f20cf3ae574d61cc33b862dbb21c30c2d136cbee
brad <lyman.brad3211@gmail.com>
2015-12-19 18:28:38 -0500

    player manager simplified and streamlined

--- src/Main.py
+++ src/Main.py
@@ -1,7 +1,19 @@
-from Components import *
-from GameInitializer import GameInitializer
-from DayActionRules import process
+from PlayerManager import *
 
-gi = GameInitializer()
-pm = gi.pm
-process(pm)
+class Name:
+    typeName = "Name"
+
+    def __init__(self, value):
+        self.value = value
+
+    def __str__(self):
+        return self.value
+
+pm = PlayerManager()
+
+p1 = pm.createPlayer()
+
+print(pm.hasComponent(p1, Name.typeName))
+pm.setComponent(p1, Name.typeName, Name("fred"))
+
+print(pm.getComponent(p1, Name.typeName))
--- src/PlayerManager.py
+++ src/PlayerManager.py
@@ -9,23 +9,22 @@ class PlayerManager:
         self.nextPId += 1
         return pId
 
-    def playerHasComponent(self, pId, comp):
-        compName = comp.name
+    def hasComponent(self, pId, compName):
         if compName not in self.components:
             return False
         if pId not in self.components[compName]:
             return False
         return True
 
-    def setComponent(self, pId, comp):
-        compName = comp.name
+    def setComponent(self, pId, compName, value):
         if compName not in self.components:
             self.components[compName] = {}
-        self.components[compName][pId] = comp
+        self.components[compName][pId] = value
 
-    def getComponent(self, pId, comp):
-        compName = comp.name
+    def getComponent(self, pId, compName):
         return self.components[compName][pId]
 
-    def getPlayersWithComponent(self, comp):
-        return self.components[comp.name]
+    def getComponentDirectory(self, compName):
+        if compName not in self.components:
+            return {}
+        return self.components[compName]
